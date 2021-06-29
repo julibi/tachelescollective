7803729d32d075f462a814a5ed5533da100c4dc9
julibi <julescodes513@egmail.com>
2021-06-29 12:36:18 +0200

    refactor(withAuthorization): use hooks and functional component

--- src/Components/Session/withAuthorization.js
+++ src/Components/Session/withAuthorization.js
@@ -1,42 +1,50 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import AuthUserContext from './context';
-import { withFirebase } from '../Firebase/context';
+import { useFirebase } from '../Firebase/context';
  
 // refactor this whole component to work with Hooks!
+// then think about what kind of custom context this useAuthorization hook should 
+// look like, if it is not a HOC - maybe it can stay a HOC
+// what is the difference between the two histories?
 
+
+// this component protects routes, so only people with access to a route are allowed
 const withAuthorization = condition => Component => {
-  class WithAuthorization extends React.Component {
-    componentDidMount() {
-      this.listener = this.props.firebase.auth.onAuthStateChanged(
+  const WrappedComponent = () => {
+    const firebase = useFirebase();
+    const history = useHistory();
+    let listener;
+
+    useEffect(() => {
+      listener = firebase.auth.onAuthStateChanged(
         authUser => {
           if (!condition(authUser)) {
-            this.props.history.push('/login');
+            history.push('/login');
           }
         },
       );
-    }
+    }, []);
+
  
-    componentWillUnmount() {
-      this.listener();
-    }
- 
-    render() {
+    useEffect(() => {
+      return () => {
+        listener();
+      }
+    }, []);
+      
+    
+
       return (
         <AuthUserContext.Consumer>
           {authUser =>
-            condition(authUser) ? <Component {...this.props} /> : null
+            condition(authUser) ? <Component /> : null
           }
       </AuthUserContext.Consumer>
       );
-    }
   }
  
-  return compose(
-    withRouter,
-    withFirebase,
-  )(WithAuthorization);
+  return WrappedComponent;
 };
  
 export default withAuthorization;
